4b3c862e20549b23ed58a8765dee81612f35308a
Cassandra Rossall <cassandrarossall@Cassandras-MacBook-Pro.local>
2020-02-11 16:37:15 +1300

    movieList component

---
+++ src/components/MovieList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+import placeholder from '../images/placeholder.png';
+import StarRating from './StarRating';
+
+const substringOverview = (desc, limit= 80) => {
+    const fullDesc = [];
+    if(desc.length > limit) {
+        desc.split(' ').reduce((acc, cur) => {
+    if(acc + cur.length <= limit) {
+        fullDesc.push(cur);
+    }
+    return acc + cur.length;
+    }, 0);
+    return `${fullDesc.join(' ')}..`;}
+    return desc;
+}
+
+const ListView = ({movies}) => {
+const MovieViewListing = movies.map(movie => {
+return movies.length ? (
+    <div className='movieList' key={movie.id}>
+        <div className = 'movie-content'>
+        <NavLink to ={`/` + movie.id}>
+            { movie.poster_path !== null ? 
+            <img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} className='movie-img' alt={movie.original_title}/>
+            : <img src={placeholder} className='movie-img' alt={movie.original_title}/>}
+        </NavLink>
+            <div className="about-movie">
+                <h4 className = 'movie-title'>{movie.original_title}</h4>
+                <p className = 'movie-overview'>{substringOverview(movie.overview)}</p>
+                <StarRating movie = {movie} />
+            </div>
+                {props.children}   
+            </div>
+		</div>
+
+) :  <div className='movieList'>
+        <h6> 'Oops. Where did all the movies ago? ' </h6>
+    </div>
+})
+ 
+    return (
+        <>
+            <div className = 'movieListView'>
+                { MovieViewListing }
+            </div>
+        </>
+    )
+    }
+
+export default ListView;
