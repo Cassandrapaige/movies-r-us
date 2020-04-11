6ea61ab026cc81f70143570e0da8023f541b4a28
Cassandra Rossall <cassandrarossall@gmail.com>
2020-04-11 15:56:57 +1200

    converted Navbar component to a functional component and created function to handle multi-word queries

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,84 +1,64 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { withRouter } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
 import Search from '../search-bar/search-bar.component'
 
 import './navbar.styles.scss'
+const joinQuery = query => query.split(' ').join('&');
 
-class Navbar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: false,
-            value: '',
-            activate_search: false
-        }
-    }
+const Navbar = (props) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [value, setValue] = useState('')
+    const [isVisible, setIsVisible] = useState(false)
 
-    toggleClass = () => {
-        this.setState({
-            active: !this.state.active
-        })
-    }
+    const toggleClass = () => setIsOpen(!isOpen)
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.history.push({
+    const handleSubmit = event => {
+        event.preventDefault()
+        props.history.push({
             pathname: '/search',
-            search: this.state.value
+            search: joinQuery(value)
         })
-        this.setState({
-            value: '',
-            active: !this.state.active
-        })
-        window.location.reload();
-    }
-    
-    handleChange = (e) => {
-        this.setState({
-            value: e.target.value
-        })
+        setValue('')
+        setIsOpen(!isOpen)
+        window.location.reload()
     }
 
-    toggleSearch = () => {
-        this.setState({
-            activate_search: !this.state.activate_search
-        })
-    }
+    const handleChange = event => { setValue(event.target.value); setIsVisible(!isVisible)}
 
-    render() {
-        return (
+    const toggleSearch = () => setIsVisible(!isVisible)
+
+    return (
         <nav className='navbar'>
         <div className="hamburger_menu">
-            <a href='#' onClick= {this.toggleClass}>
-               {this.state.active ? <i class="fas fa-times"></i> :
+            <a href='#' onClick= {toggleClass}>
+               {isOpen ? <i class="fas fa-times"></i> :
                <i className="fas fa-bars"></i>
                } </a>
         </div>
             <NavLink to= '/' className='navbar-brand'>Movies R Us</NavLink>
-            <ul className="navbar-nav" id={this.state.active ? "show" : null}>
-                <li className="navbar-item" onClick= {this.toggleClass}>
+            <ul className="navbar-nav" id={isOpen ? "show" : null}>
+                <li className="navbar-item" onClick= {toggleClass}>
                     <NavLink to= '/new' className='nav-link'>Now Playing</NavLink>
                 </li>
-                <li className="navbar-item" onClick= {this.toggleClass}>
+                <li className="navbar-item" onClick= {toggleClass}>
                     <NavLink to= '/popular' className='nav-link'>Most Popular</NavLink>
                 </li>
-                <li className="navbar-item" onClick= {this.toggleClass}>
+                <li className="navbar-item" onClick= {toggleClass}>
                     <NavLink to= '/top-rated' className='nav-link'>Top Rated</NavLink>
                 </li>
                 <li className="navbar-item">
                     <Search 
-                        activate_search = {this.state.activate_search}
-                        toggleSearch = {this.toggleSearch}
-                        onChange = {this.handleChange}
-                        handleSubmit = {this.handleSubmit}
-                        value = {this.state.value}
+                        isVisible = {isVisible}
+                        toggleSearch = {toggleSearch}
+                        handleChange = {handleChange}
+                        handleSubmit = {handleSubmit}
+                        value = {value}
                     />
                 </li>
             </ul>
         </nav>
-        )
-    }
-}
+
+    )}
 
 export default withRouter(Navbar);
--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 import './search-bar.styles.scss'
 
-const Search = (props) => {
+const Search = ({isVisible, toggleSearch, handleChange, handleSubmit, value}) => {
     return (
-        <form className = {props.activate_search ? 'expand_search search_form' : 'search_form' } onSubmit={props.handleSubmit}>
+        <form className = {`search_form ${isVisible ? 'expand_search' : ''}`} onSubmit={handleSubmit}>
             <div className="input-field">
                 <input 
-                    onClick = {props.toggleSearch}
+                    onClick = {toggleSearch}
                     type="text" 
                     placeholder='&#xF002; Search movie' 
-                    onChange={props.onChange} 
-                    value = {props.value}/>            
+                    onChange={handleChange} 
+                    value = {value}/>            
             </div>
         </form>
     )
