f87855e2097414c5d88d237e4f2655a283411eed
Cassandra Rossall <cassandrarossall@gmail.com>
2021-02-05 12:01:20 +1300

    added scroll to top on pagination click

--- src/components/fetched-results/fetched-results.component.jsx
+++ src/components/fetched-results/fetched-results.component.jsx
@@ -1,71 +1,83 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import axios from 'axios'
-import {withRouter} from 'react-router-dom'
-import * as QueryString from "query-string"
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import { withRouter } from "react-router-dom";
+import * as QueryString from "query-string";
 
-import Pagination from '../pagination/pagination.component'
-import MovieView from '../movie-view/movie-view.component'
-import ErrorMessage from '../error-message/error-message.component'
-import BackButton from '../back-button/back-button.component'
+import Pagination from "../pagination/pagination.component";
+import MovieView from "../movie-view/movie-view.component";
+import ErrorMessage from "../error-message/error-message.component";
+import BackButton from "../back-button/back-button.component";
 
-const FetchedResults = ({url, genre, error, location, history, match, path, ...props}) => {
-    const params = QueryString.parse(location.search);
-    const [movies, setMovies] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [current, setCurrent] = useState(1);
-    const [total, setTotal] = useState()     
+const FetchedResults = ({
+  url,
+  genre,
+  error,
+  location,
+  history,
+  match,
+  path,
+  ...props
+}) => {
+  const params = QueryString.parse(location.search);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [current, setCurrent] = useState(1);
+  const [total, setTotal] = useState();
 
-    const joinQuery = query => query.replace(/-/g,'&').split(' ').join('&').toLowerCase();
+  const joinQuery = (query) =>
+    query.replace(/-/g, "&").split(" ").join("&").toLowerCase();
 
-    useEffect(() => {
-        if(location.pathname === "/search") setCurrent(Number(params.page));
-        else setCurrent(Number(match.params.page.match(/\d+/)[0]))
-    }, [location.pathname]);
+  useEffect(() => {
+    if (location.pathname === "/search") setCurrent(Number(params.page));
+    else setCurrent(Number(match.params.page.match(/\d+/)[0]));
+  }, [location.pathname, params.page, match.params.page]);
 
-    const fetchResults = useCallback(async () => {
-        const result = await axios.get(`${url}&page=${current}`);
-        setMovies(result.data.results)
-        const total_results = await result.data.total_results;
-        setTotal(total_results);
-        setIsLoading(false);
-    }, [current, url]);
+  const fetchResults = useCallback(async () => {
+    const result = await axios.get(`${url}&page=${current}`);
+    setMovies(result.data.results);
+    const total_results = await result.data.total_results;
+    setTotal(total_results);
+    setIsLoading(false);
+  }, [current, url]);
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-        setIsLoading(true);
-        fetchResults();
-    },[fetchResults])
-  
-    const next = num => {
-        let routeUrl;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setIsLoading(true);
+    fetchResults();
+  }, [fetchResults]);
 
-        if(location.pathname === "/search") {
-            routeUrl = `${joinQuery(location.pathname)}${location.search}`;
-        } else routeUrl = location.pathname;
+  const next = (num) => {
+    let routeUrl;
 
-        const newRoute = routeUrl.slice(0, routeUrl.lastIndexOf("&"));
-        setCurrent(num);
-        history.push(`${newRoute}&page=${num}`);
-    }
+    if (location.pathname === "/search") {
+      routeUrl = `${joinQuery(location.pathname)}${location.search}`;
+    } else routeUrl = location.pathname;
 
-    const numPages = Math.floor(total / 20)
+    const newRoute = routeUrl.slice(0, routeUrl.lastIndexOf("&"));
+    setCurrent(num);
+    history.push(`${newRoute}&page=${num}`);
+    window.scrollTo(0, 0);
+  };
 
-    return (
-        total !== 0 || undefined ? 
-            <MovieView 
-                movies = {movies} 
-                isLoading = {isLoading} 
-                total = {total} 
-                subtext = "Click on an image to read more or see movies that are similar"
-                {...props}>
-                {total > 20 &&
-                <Pagination pages= {numPages} next={next} current = {current}/> }
-            </MovieView>
-            :
-            <ErrorMessage error = {error}> 
-                <BackButton />
-            </ErrorMessage>
-    )
-}         
-    
+  const numPages = Math.floor(total / 20);
+
+  return total !== 0 || undefined ? (
+    <MovieView
+      movies={movies}
+      isLoading={isLoading}
+      total={total}
+      subtext='Click on an image to read more or see movies that are similar'
+      {...props}
+    >
+      {total > 20 && (
+        <Pagination pages={numPages} next={next} current={current} />
+      )}
+    </MovieView>
+  ) : (
+    <ErrorMessage error={error}>
+      <BackButton />
+    </ErrorMessage>
+  );
+};
+
 export default withRouter(FetchedResults);
--- src/pages/genre-page.component.jsx
+++ src/pages/genre-page.component.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import {withRouter} from 'react-router-dom'
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-import {API_KEY} from '../base'
+import { API_KEY } from "../base";
 
-import {GENRE_DATA} from '../constants'
+import withVideo from "../withVideo";
 
-import withVideo from '../withVideo'
+import FetchedResults from "../components/fetched-results/fetched-results.component";
 
-import FetchedResults from '../components/fetched-results/fetched-results.component'
+const GenrePage = ({ action, match, history }) => {
+  const id = Number(match.params.id);
 
-const GenrePage = ({action, match, history}) => {
-    const id = Number(match.params.id);
-    
-    return (
-        <FetchedResults
-            action = {action}
-            title = {match.params.genre}
-            url = {`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${id}`} />
-    )
-}   
+  return (
+    <FetchedResults
+      action={action}
+      title={match.params.genre}
+      url={`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${id}`}
+    />
+  );
+};
 
-export default withVideo(withRouter(GenrePage))
+export default withVideo(withRouter(GenrePage));
